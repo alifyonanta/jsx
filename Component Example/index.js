b4ccc230dbed452487b537e249d36ce44f5f9279
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2022-02-18 17:48:40 +0700

    Update index.js
    Add state, hooks, lifecycle methods and handling events

--- Component Example/index.js
+++ Component Example/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
@@ -47,7 +47,7 @@ function Item(props) {
   </div>;
 }
 
-function App() {
+function App2() {
   return <div>
     <Item name="Cheese" price="4.99" />
     <Item name="Bread" price="1.5" />
@@ -55,8 +55,184 @@ function App() {
   </div>;
 }
 
-const el = <App />; 
+const el4 = <App2 />; 
 ReactDOM.render(
-  el, 
+  el4, 
   document.getElementById('rootItem')
+);
+
+//STATE
+class Counter extends React.Component {
+  state = {
+    counter: 0
+  }
+  increment = () => {
+    this.setState({counter: this.state.counter+1});
+  }
+  render() {
+    return <div>
+    <p>{this.state.counter}</p>
+    <button onClick={this.increment}>Increment</button>
+    </div>;
+  }
+}
+const e15 = <Counter />; 
+ReactDOM.render(e15, document.getElementById('rootState')
+);
+
+//HOOKS
+function Counter2() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter+1);
+  }
+  return <div>
+  <p>{counter}</p>
+  <button onClick={increment}>Increment</button>
+  </div>;
+}
+const el6 = <Counter2 />; 
+ReactDOM.render(
+  el6, 
+  document.getElementById('rootHooks')
+);
+
+function HelloHooks() {
+  const [name, setName] = useState("David");
+
+  return <h1>Hello {name}.</h1>;
+}
+const el7 = <HelloHooks />; 
+ReactDOM.render(
+  el7, 
+  document.getElementById('rootHooks2')
+);
+
+//Lifecycle Methods
+class Counter3 extends React.Component {
+  state = {
+    counter: 0
+  }
+  increment = () => {
+    this.setState({counter: this.state.counter+1});
+  }
+  componentDidMount() {
+    this.setState({counter: 42});
+  }
+  render() {
+    return <div>
+    <p>{this.state.counter}</p>
+    <button onClick={this.increment}>Increment</button>
+    </div>;
+  }
+}
+const el8 = <Counter3 />; 
+ReactDOM.render(
+  el8, 
+  document.getElementById('rootLife')
+);
+
+class Counter4 extends React.Component {
+  state = {
+    counter: 0
+  }
+  increment = () => {
+    this.setState({counter: this.state.counter+1});
+  }
+  componentDidUpdate() {
+    alert("Number of clicks: " + this.state.counter);
+  }
+  render() {
+    return <div>
+    <p>{this.state.counter}</p>
+    <button onClick={this.increment}>Increment</button>
+    </div>;
+  }
+}
+const el9 = <Counter4 />; 
+ReactDOM.render(
+  el9, 
+  document.getElementById('rootLife2')
+);
+
+// function Counter5() {
+//   const [counter, setCounter] = useState(0);
+
+//   useEffect(() => {
+//     alert("Number of clicks: " + counter);
+//   });
+
+//   function increment() {
+//     setCounter(counter+1);
+//   }
+//   return <div>
+//   <p>{counter}</p>
+//   <button onClick={increment}>Increment</button>
+//   </div>;
+// }
+// const e20 = <Counter5 />; 
+// ReactDOM.render(
+//   e20, 
+//   document.getElementById('rootLife3')
+// );
+
+//Handling Events
+function Toggle() {
+  const [val, setVal] = useState("ON");
+  function toggle() {
+    setVal((val=="ON")?"OFF":"ON");
+  }
+  return <button onClick={toggle}>{val}</button>;
+}
+const e21 = <Toggle />; 
+ReactDOM.render(
+  e21, 
+  document.getElementById('rootHandling')
+);
+
+function Converter() {
+  const [km, setKm] = useState(0);
+
+  function handleChange(e) {
+    setKm(e.target.value);
+  }
+  function convert(km) {
+    return (km/1.609).toFixed(2);
+  }
+
+  return <div>
+  <input type="text" value={km} onChange={handleChange} />
+  <p> {km} km is {convert(km)} miles </p>
+  </div>;
+}
+const e22 = <Converter />; 
+ReactDOM.render(
+  e22, 
+  document.getElementById('rootHandling2')
+);
+
+function AddForm() {
+  const [sum, setSum] = useState(0);
+  const [num, setNum] = useState(0);
+
+  function handleChange(e) {
+    setNum(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    setSum(sum + Number(num));
+    e.preventDefault();
+  }
+
+  return <form onSubmit={handleSubmit}>
+  <input type="number" value={num} onChange={handleChange} />
+  <input type="submit" value="Add" />
+  <p> Sum is {sum} </p>
+  </form>;
+}
+const e23 = <AddForm />; 
+ReactDOM.render(
+  e23, 
+  document.getElementById('rootHandling3')
 );
